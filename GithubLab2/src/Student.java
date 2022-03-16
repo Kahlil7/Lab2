1b054d818103a62e198d6802b01efc032af8a950
admin <admin@Bcalab4-m06>
2022-03-16 10:24:41 +0530

    commit

--- GithubLab2/src/Student.java
+++ GithubLab2/src/Student.java
@@ -1,9 +1,14 @@
 
 public class Student {
-
+	String name;
+	Student(String name)
+	{
+		this.name = name;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Students");
+		System.out.println("Hello Students");
+		Student S1 = new Student("lia");
+		System.out.println(S1.name);
 	}
-
 }
